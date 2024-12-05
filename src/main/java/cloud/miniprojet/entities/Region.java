55aeddff6811c02165c3ce482d8e84142ce7d8ec
Syrine <syrineayeb1@gmail.com>
2024-12-05 01:26:58 +0100

    first commit

---
+++ src/main/java/cloud/miniprojet/entities/Region.java
@@ -0,0 +1,19 @@
+package cloud.miniprojet.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Data // Generates getters, setters, toString, equals, and hashCode methods
+@NoArgsConstructor // Generates a no-argument constructor
+@AllArgsConstructor // Generates a constructor with all arguments
+public class Region {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String libelle;
+
+
+}
